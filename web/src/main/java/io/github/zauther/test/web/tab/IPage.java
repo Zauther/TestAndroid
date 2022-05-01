ebd31e5215f2f013b26cdfe23fa6fc7ccc468ffa
zauther <zauther@qq.com>
2022-05-01 14:44:17 +0800

    test tab

---
+++ web/src/main/java/io/github/zauther/test/web/tab/IPage.java
@@ -0,0 +1,5 @@
+package io.github.zauther.test.web.tab;
+
+public interface IPage extends IView{
+    ITab[] getChildren();
+}
---
+++ web/src/main/java/io/github/zauther/test/web/tab/IView.java
@@ -0,0 +1,8 @@
+package io.github.zauther.test.web.tab;
+
+import android.view.View;
+
+public interface IView extends ITab{
+
+    View getView();
+}
